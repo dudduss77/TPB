46a4e4f057c66b7ba7068676281bbd67a1738adc
Damian Karbowiak <42693193+dudduss77@users.noreply.github.com>
2020-11-14 15:26:16 +0100

    Nav component end with children

---
+++ src/components/singOutComponent/SingOutComponent.js
@@ -0,0 +1,14 @@
+import React from "react"
+import './SingOutComponent.scss'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const SingOutComponent = () => {
+  return (
+    <div className="singOutComponent">
+      <FontAwesomeIcon icon="sign-out-alt"/>
+    </div>
+  )
+}
+
+export default SingOutComponent;
--- src/fontLibrary.js
+++ src/fontLibrary.js
@@ -1,4 +1,4 @@
 import {library} from '@fortawesome/fontawesome-svg-core';
-import { faBell, faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faBell, faPlus, faHome,  faTasks, faDollarSign, faBullseye, faSignOutAlt} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faBell, faPlus)
+library.add(faBell, faPlus, faHome,  faTasks, faDollarSign, faBullseye, faSignOutAlt)
